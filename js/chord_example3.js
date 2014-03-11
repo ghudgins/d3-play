a151e7c68a02c8087242b0de11b6157f70f8d137
Graham Hudgins <graham.hudgins@gmail.com>
2014-03-10 20:39:17 -0400

    Expanded the chord functionality

--- js/chord_example3.js
+++ js/chord_example3.js
@@ -1,19 +1,28 @@
 
+var fill2;
+
 //  CREATE MATRIX AND MAP
 
 d3.csv('data/walmartcooccurrence3.csv', function (error, data) {
   var mpr = chordMpr(data);
+  
+  _.each(data, function (d) { //A
+    mpr.addToMap(d.PRIMARY_CAT, d.SENTIMENT)
+  });
 
-  mpr
-    .addValuesToMap('PRIMARY_CAT')
-    .setFilter(function (row, a, b) {
-      return (row.PRIMARY_CAT === a.name && row.REL_CAT === b.name);
+  mpr.setFilter(function (row, a, b) {
+      return (row.PRIMARY_CAT === a.name && row.REL_CAT === b.name)
     })
-    .setAccessor(function (recs, a, b) {
+    .setAccessor(function (recs, a, b) { //B
       if (!recs[0]) return 0;
-      return +recs[0].COOC;
+      return {sentiment: recs[0].SENTIMENT, 
+              volume: recs[0].COOC, 
+              valueOf: value
+            };
     });
-  drawChords(mpr.getMatrix(), mpr.getMap());
+  drawChords(mpr.getMatrix(), mpr.getMap(), 0);
+
+  function value() { return +this.volume; } //C
 });
 
 //  DRAW THE CHORD DIAGRAM
@@ -24,17 +33,51 @@ function drawChords (matrix, mmap) {
   var fill = 
       //d3.scale.category20c();
       d3.scale.ordinal()
-      .domain(d3.range(5))
+      .domain(d3.range(4))
       .range([//"#69D2E7", "#A7DBD8", "#E0E4CC", "#F38630", "#FA6900" //goldfish
-        "#FF9900", "#424242", "#E9E9E9", "#BCBCBC", "#3299BB" //gamebrooks
-        //"#594F4F", "#547980", "#45ADA8", "#9DE0AD", "#E5FCC2", //greens
-        //"#FE4365", "#FC9D9A", "#F9CDAD", "#C8C8A9", "#83AF9B", //candy
-        //"#373737", "#99ADB6", "#CFD9B6", "#E8E6C0" //manly colors
+        //"#FF9900", "#424242", "#E9E9E9", "#BCBCBC", "#3299BB" //gamebrooks
+        //"#594F4F", "#547980", "#45ADA8", "#9DE0AD", "#E5FCC2" //greens
+        //"#FE4365", "#FC9D9A", "#F9CDAD", "#C8C8A9", "#83AF9B" //candy
+        "#373737", "#99ADB6", "#CFD9B6", "#E8E6C0" //manly colors
         //"#F4E3DB", "#F0CBC3", "#F8A2A1", "#F6878E", "#CA9FA6" //pinks
         ]);
 
+    fill2 = function(sentiment, returnText) {
+    if (!returnText) {
 
-   var chord = d3.layout.chord()
+      if (sentiment+0 < -1) {
+        return "#8C2318";
+      } else if (sentiment+0 >= -1 && sentiment+0 < -0.40) {
+        return "#F2C45A";
+      } else if (sentiment+0 >= -0.40 && sentiment+0 < 0.40) {
+        return "#BFB35A";
+      } else if (sentiment+0 >= 0.40 && sentiment+0 < 1) {
+        return "#88A65E";
+      } else {
+        return "#5E8C6A";
+      };
+
+    } else {
+
+      if (sentiment+0 < -1) {
+        return "Negative";
+      } else if (sentiment+0 >= -1 && sentiment+0 < -0.40) {
+        return "Somewhat Negative";
+      } else if (sentiment+0 >= -0.40 && sentiment+0 < 0.40) {
+        return "Neutral or Polarized";
+      } else if (sentiment+0 >= 0.40 && sentiment+0 < 1) {
+        return "Somewhat Positive";
+      } else {
+        return "Positive";
+      };
+
+
+    }
+
+  }
+
+
+  var chord = d3.layout.chord()
       .padding(.02)
       .sortSubgroups(d3.ascending)
       //.sortChords(d3.descending)
@@ -90,8 +133,8 @@ function drawChords (matrix, mmap) {
           .enter()
           .append("svg:path")
           .attr("class", "chord")
-          .style("stroke", function(d) { return d3.rgb(fill(d.target.index)).darker(); })
-          .style("fill", function(d) { return fill(d.target.index); })
+          .style("stroke", function(d) { return d3.rgb(fill2(+rdr(d).sdata.sentiment)).darker(); })
+          .style("fill", function(d) { return fill2(+rdr(d).sdata.sentiment); })
           .attr("d", d3.svg.chord().radius(r0))
           .on("mouseover", function (d) {
             d3.select("#tooltip")
@@ -105,7 +148,8 @@ function drawChords (matrix, mmap) {
 
     function chordTip (d) {
       var p = d3.format(".2%"), q = d3.format(",r")
-      return "<strong>" + q(d.svalue) + "</strong><i> customers talk about both</i> <strong>" + d.sname + "</strong><i> and </i><strong>" + d.tname + "</strong><br/><br/>"
+      return "<strong>" + q(d.svalue) + "</strong><i> customers talk about both</i> <strong>" + d.sname + "</strong><i> and </i><strong>" + d.tname + "</strong><br/>"
+        + "<i>with a </i><strong><font color=" + fill2(+d.sdata.sentiment) + ">" + fill2(+d.sdata.sentiment, 1) + "</font></strong><i> average sentiment</i><br/><br/>"
         + "<i>this is...</i><br/>" 
         + "<strong>" + p(d.svalue/d.stotal) + " </strong><i>of comments about </i>" + "<strong>" + d.sname + "</strong> <i>and</i> "
         + (d.sname === d.tname ? "": ("<br/><strong>" + p(d.tvalue/d.ttotal) + " </strong><i>of comments about </i>" + "<strong>" + d.tname + "</strong> "));
